8e5ffd8e7207c677a684d0e3431f3ee18ac923ec
atpugvaraa <atpugvaraa@gmail.com>
2025-03-01 21:06:34 +0530

    Initial Commit

---
+++ addictshun/addictshunApp.swift
@@ -0,0 +1,17 @@
+//
+//  addictshunApp.swift
+//  addictshun
+//
+//  Created by Aarav Gupta on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct addictshunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
